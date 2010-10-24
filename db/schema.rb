15c3a5404a189efa68850dc58134a7055c0883e9
numsweb <numsweb@gmail.com>
2010-10-24 00:01:09 -0500

    add db schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,31 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101023233845) do
+ActiveRecord::Schema.define(:version => 20101024044425) do
+
+  create_table "books", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "paragraphs", :force => true do |t|
+    t.text     "body_text"
+    t.integer  "story_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.string   "title"
+  end
+
+  create_table "stories", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
